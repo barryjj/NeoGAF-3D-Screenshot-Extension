64f7df89608516845ac51b16e57acae27cba14a6
Jason Barry <barryjj@gmail.com>
2016-12-19 16:01:49 -0500

    Reworked content.js, just needs element insertion to be handled.

--- content.js
+++ content.js
@@ -1,39 +1,42 @@
+'use strict'
+
+//  General psuedocode to get the ball rolling:
+//  
+//      If node is an anchor tag we need to do the following:
+//          1. grab the href attribute
+//          2. Match it against known types
+//          3. Generate the appropriate inline for embedding
+//          4. Replace the href with the embed code
+//  
+//  kuula.co embeds look like this:
+//  <iframe width="640px" height="400px" src="https://kuula.co/share/7fBQk" allowfullscreen="true" frameborder="0" scrolling="no"></iframe>
+
+function embed3dScreenshots(){
+    //set the filter key to kuula.co
     var filterkey = "kuula.co";
-    var allElements, thisElement, currentHref;
-
-            //  if node is an anchor tag we need to do the following:
-            //  1. grab the href attribute
-            //  2. Match it against known types
-            //  3. Generate the appropriate inline for embedding
-            //  4. Replace the href with the embed code
-            //  
-            //  Note to self: check to see what the hosts are for the viewers and if their embeds can be
-            //  made into a boilerplate for replacement instead of having to have cases.
-            //  In the worst instance, I suppose we could have a host check once the href value is checked
-            //  to determine which template for inline to use.
-
     //get all a tags
-    allElements = document.getElementsByTagName('a');
-    
+    var links = document.querySelectorAll('.post a');
+    //variables to be used later for storing href value and creating the screenshot to embed
+    var screenshot, link;
+ 
     //for each anchor tag
-    for (var i = 0; i < allElements.length; i++) {
-        thisElement = allElements[i];
+    for (var i = 0; i < links.length; i++) {
+        
+        //store the current href value
+        link = links[i].href;
 
-        //Get the current anchor's href value
-        currentHref = thisElement.getAttribute(href);
-
-        //if the current href value has the kuula.co in it
-        if (currentHref.indexOf(filterkey) != -1) {
-
-            //replace the current element with the iframe
-            thisElement.parentNode.style.display = 'none';
-        }
+        //if the href contains the filterkey, or kuula.co
+        if (link.indexOf(filterkey) != -1) {
+            screenshot = document.createElement('iframe');
+            screenshot.src = link;
+            screenshot.width="640px";
+            screenshot.height="400px";
+            screenshot.allowfullscreen="true";
+            screenshot.frameborder="0";
+            screenshot.scrolling="no";
             
-            /*
             //<iframe width="640px" height="400px" src="https://kuula.co/share/7fBQk" allowfullscreen="true" frameborder="0" scrolling="no"></iframe>
-
-             */
-            }
-        }
     }
 }
+//In doing some research, this might help the extension to work with the LiveThreadUpdate plugin
+//document.addEventListener('LiveThreadUpdate', embed3dScreenshots);
