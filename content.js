3d516a2bac95ae155f44ba506096577ff5978e53
Jason Barry <barryjj@gmail.com>
2016-12-19 15:28:34 -0500

    Reworked framework, almost ready to test

--- content.js
+++ content.js
@@ -1,18 +1,8 @@
-var elements = document.getElementsByTagName('*');
-
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
-
-        //Select (1) elements, not (2) attributes or (3) text
-        if (node.nodeType === 1) {
-            var curElement = node.nodeValue;
-            
+    var filterkey = "kuula.co";
+    var allElements, thisElement, currentHref;
 
             //  if node is an anchor tag we need to do the following:
-            //  1. Grab the href attribute
+            //  1. grab the href attribute
             //  2. Match it against known types
             //  3. Generate the appropriate inline for embedding
             //  4. Replace the href with the embed code
@@ -22,6 +12,27 @@ for (var i = 0; i < elements.length; i++) {
             //  In the worst instance, I suppose we could have a host check once the href value is checked
             //  to determine which template for inline to use.
 
+    //get all a tags
+    allElements = document.getElementsByTagName('a');
+    
+    //for each anchor tag
+    for (var i = 0; i < allElements.length; i++) {
+        thisElement = allElements[i];
+
+        //Get the current anchor's href value
+        currentHref = thisElement.getAttribute(href);
+
+        //if the current href value has the kuula.co in it
+        if (currentHref.indexOf(filterkey) != -1) {
+
+            //replace the current element with the iframe
+            thisElement.parentNode.style.display = 'none';
+        }
+            
+            /*
+            //<iframe width="640px" height="400px" src="https://kuula.co/share/7fBQk" allowfullscreen="true" frameborder="0" scrolling="no"></iframe>
+
+             */
             }
         }
     }
